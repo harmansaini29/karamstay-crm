751d1dc2aa47f71bddaf5bb8cef7c5117d08a0b8
harmansaini29 <harmansinghsaini4134@gmail.com>
2026-09-07 22:30:08 +0530

    fix(infra): set ECR tag mutability to MUTABLE and sync workflow

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -386,7 +386,7 @@ resource "aws_ssm_parameter" "external_secret" {
 
 resource "aws_ecr_repository" "app" {
   name                 = "${local.name_prefix}-backend"
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "MUTABLE"
   image_scanning_configuration { scan_on_push = true }
 }
 
