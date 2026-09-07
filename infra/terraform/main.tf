a26d1a191820f23c33d4eae500a163231804ca21
harmansaini29 <harmansinghsaini4134@gmail.com>
2026-09-07 19:35:41 +0530

    ci: update OIDC thumbprint and support both vars and secrets in GitHub Actions

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -520,7 +520,11 @@ resource "aws_iam_role_policy_attachment" "ecs_task" {
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
+  thumbprint_list = [
+    "227203b5317f3818cab5b5ce596132bf36748c0e",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+  ]
 }
 
 data "aws_iam_policy_document" "github_actions_assume" {
